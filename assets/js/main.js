86406239015b2d475147e792bc06bb94d0a85a09
Aragorn Dai <tzjie.dai@gmail.com>
2021-11-11 21:18:07 +0800

    Design the about section.

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function getAge(year, month, day) {
+	var today = new Date();
+	var birthday = new Date(year, month - 1, day);
+	var y = today.getFullYear() - birthday.getFullYear();
+	var m = today.getMonth() - birthday.getMonth();
+	var d = today.getDate() - birthday.getDate();
+	if (m < 0 || (m === 0 && d < 0)) {
+		y--;
+	}
+	return y;
+}
