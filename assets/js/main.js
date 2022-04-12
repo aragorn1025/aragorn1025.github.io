c0bc6f40f16434cc561d6a780fe18ea7d1089e70
Aragorn Dai <tzjie.dai@gmail.com>
2022-04-12 22:59:44 +0800

    Add last updated date.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,41 @@ function getAge(year, month, day) {
   return y;
 }
 
+function getDateString(date) {
+  return ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2) + "-" + date.getFullYear();
+}
+
+function getDayDiffString(date) {
+  var today = new Date();
+  var diff = today - date;
+  if (diff <= 0) {
+    diff = 0;
+  }
+  diff = Math.floor(diff / 1000 / 60 / 60);
+  if (diff < 24) {
+    return diff + " hour" + (diff <= 1 ? "" : "s")
+  }
+  diff = Math.floor(diff / 24);
+  if (diff < 7) {
+    return diff + " day" + (diff <= 1 ? "" : "s")
+  }
+  if (diff < 30) {
+    diff = Math.floor(diff / 7);
+    return diff + " week" + (diff <= 1 ? "" : "s")
+  }
+  if (diff < 365) {
+    diff = Math.floor(diff / 30);
+    return diff + " month" + (diff <= 1 ? "" : "s")
+  }
+  diff = Math.floor(diff / 365);
+  return diff + " year" + (diff <= 1 ? "" : "s")
+}
+
+function getLastUpdatedString(year = 2022, month = 4, day = 12, hour = 23) {
+  var date = new Date(year, month - 1, day, hour, 0, 0);
+  return "Last updated at " + getDateString(date) + " (" + getDayDiffString(date) + " ago).";
+}
+
 function setTooltip(target) {
   $(target).prop("title", function () {
     return $(this).text().replaceAll("\t", "").replaceAll("\n", " ");
