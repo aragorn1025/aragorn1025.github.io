b38e607a54f5d85d66977ee4c458622ced4f8585
Aragorn Dai <tzjie.dai@gmail.com>
2022-04-12 06:15:10 -0700

    Use prettier to keep the format.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 function getAge(year, month, day) {
-	var today = new Date();
-	var birthday = new Date(year, month - 1, day);
-	var y = today.getFullYear() - birthday.getFullYear();
-	var m = today.getMonth() - birthday.getMonth();
-	var d = today.getDate() - birthday.getDate();
-	if (m < 0 || (m === 0 && d < 0)) {
-		y--;
-	}
-	return y;
+  var today = new Date();
+  var birthday = new Date(year, month - 1, day);
+  var y = today.getFullYear() - birthday.getFullYear();
+  var m = today.getMonth() - birthday.getMonth();
+  var d = today.getDate() - birthday.getDate();
+  if (m < 0 || (m === 0 && d < 0)) {
+    y--;
+  }
+  return y;
 }
 
 function setTooltip(target) {
-	$(target).prop("title", function () {
-		return $(this).text().replaceAll("\t", "").replaceAll("\n", " ");
-	});
+  $(target).prop("title", function () {
+    return $(this).text().replaceAll("\t", "").replaceAll("\n", " ");
+  });
 }
