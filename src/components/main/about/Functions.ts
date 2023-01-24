2d9d0dd28909835ae520cc8dcc33334d19a296b7
aragorn1025 <tzjie.dai@gmail.com>
2023-01-25 00:04:56 +0800

    style: skip `no-explicit-any` warning

--- src/components/main/about/Functions.ts
+++ src/components/main/about/Functions.ts
@@ -6,6 +6,7 @@ const getAge = (birthday: { year: number; month: number; day: number }) => {
   return y + (m < 0 || (m === 0 && d < 0) ? -1 : 0);
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getFunction: (o: any) => any = (name: string) => {
   switch (name) {
     case 'getAge':
