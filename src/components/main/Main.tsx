c17e570bf3cf51b80eddc230c20d2ec87348f2b3
aragorn1025 <tzjie.dai@gmail.com>
2026-08-02 00:38:58 -0700

    refactor: rewrite summary section

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-import aboutProps from '../../assets/data/About.json';
 import homeProps from '../../assets/data/Home.json';
+import summaryProps from '../../assets/data/Summary.json';
 
 import './Main.css';
-import AboutSection, { AboutSectionProps } from './about/AboutSection';
-import ContactSection from './contact/ContactSection';
 import HomeSection from './home/HomeSection';
 import PortfolioSection from './portfolio/PortfolioSection';
 import ResumeSection from './resume/ResumeSection';
 import SkillsSection from './skills/SkillsSection';
+import SummarySection, { SummarySectionProps } from './summary/SummarySection';
 
 const Main: React.FunctionComponent = () => {
   return (
     <main className="bg-dark bg-opacity-50 text-dark text-opacity-50">
       <HomeSection {...homeProps} />
-      <AboutSection {...(aboutProps as AboutSectionProps)} />
+      <SummarySection {...(summaryProps as SummarySectionProps)} />
       <SkillsSection />
       <ResumeSection />
       <PortfolioSection />
-      <ContactSection />
     </main>
   );
 };
